5bd55427c803a986affecd77dd2adf8d7095f986
Megha Jayalath <113291487+MeghaJayalath@users.noreply.github.com>
2026-01-13 13:05:27 +0530

    Move to a mono repo setup

--- src/types/modules.d.ts
+++
@@ -1,5 +0,0 @@
-declare module 'torrent-search-api' {
-    export function enablePublicProviders(): void;
-    export function search(query: string, category: string, limit: number): Promise<any[]>;
-    // Add other methods if needed
-}
